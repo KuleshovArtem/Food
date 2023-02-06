3cd1e4a16e77c14fe3789cd220051085b754ba68
artem kuleshov <kooleshov.artem@gmail.com>
2023-02-06 12:04:59 +0500

    отправка на сервер с помощью fetch через formdata

--- js/script.js
+++ js/script.js
@@ -249,32 +249,34 @@ window.addEventListener('DOMContentLoaded', () => {
             `; //лучше добавлять класс.
             form.insertAdjacentElement('afterend', statusMessage); //добавление на страницу 
 
-            const request = new XMLHttpRequest();
-            request.open('POST', 'server.php');
+            // const request = new XMLHttpRequest();
+            // request.open('POST', 'server.php');
 
-            request.setRequestHeader('Content-type', 'application/json');
             const formData = new FormData(form);
 
-            const object = {};
-            formData.forEach(function (value, key) {
-                object[key] = value;
-            });
+            // const object = {};
+            // formData.forEach(function (value, key) {
+            //     object[key] = value;
+            // });
 
-            const json = JSON.stringify(object);
+            // const json = JSON.stringify(object);
             
-            request.send(json);
-
-            request.addEventListener('load', () => {
-                if (request.status === 200) {
-                    console.log(request.response);
-                    showThanksModal(message.success);
-                    form.reset();
-                    statusMessage.remove();
-                } else {
-                    console.log(request.response);
-                    showThanksModal(message.failure);
-                }
+            fetch('server.php', {
+                method: 'POST',
+                body: formData,
+                //headers:{'Content-type': 'application/json'}
+            }).then(data => data.text())
+            .then(data => {
+                console.log(data);
+                showThanksModal(message.success);
+                statusMessage.remove();
+            }).catch((data) => {
+                console.log(data);
+                showThanksModal(message.failure);
+            }).finally(() => {
+                form.reset();
             });
+
         });
     }
 
